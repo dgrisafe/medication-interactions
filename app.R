250224f80d9bda45685af0a80ad5aaed52d2a6b2
Dom G <21165092+dgrisafe@users.noreply.github.com>
2023-12-29 12:38:26 -0600

    Pull packages to beginning of app so they are not run multiple times inside the server Fn Remove extraneous text Update input text for meds to indicate at least 2 meds entered Add conditional, so app doesn't run unless at least 2 medications are entered

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 library(shiny)  # http://shiny.rstudio.com/
+library(tidyverse)
+library(gtools)
 
 # input .txt file w/common psychiatric medicaions on each line
 medsPsych <- as.data.frame(read.table(file = "medicationsPsychiatry.txt", header = FALSE, sep = "\n"))
@@ -18,7 +20,7 @@ ui <- fluidPage(
         # input box to enter medications
         selectizeInput(
           inputId = "medicationsPsychiatry",
-          label = "Enter list of medications, separated by commas",
+          label = "Enter 2 or more medications, separated by commas",
           choices = medsPsych$meds,
           selected = NULL,
           multiple = TRUE,
@@ -27,7 +29,7 @@ ui <- fluidPage(
             'plugins' = list('remove_button'),
             'create' = TRUE,
             'persist' = TRUE,
-          placeholder = "e.g., abilify PO, haloperidol, quetiapine"
+          placeholder = "e.g., abilify, haloperidol, quetiapine"
           )
           ),
 
@@ -52,19 +54,7 @@ ui <- fluidPage(
       # main panel output
       mainPanel(
         
-        h2("Antipsychotic/Mood Stabilizer Polypharmacy Information Sheet"),
         p("Your treatment team may prescribe more than one medication at a time."),
-        
-        # h3("Why would I receive two similar types of medication?"),
-        # p("1. Improve or optimize your treatment by using medications that work in different ways to treat the same condition. This can be a useful strategy if one medication used alone was not adequately treating your symptoms."),
-        # p("2. Your provider may be attempting to switch from one medication to another. In order to safely switch to another medication, they may have to slowly decrease the dose of one and increase the dose of another medication."),
-        # 
-        # h3("What are the potential risks of taking two or more antipsychotic medications at the same time?"),
-        # p("- Increased sedation: Taking two or more antipsychotics can increase the amount of tiredness that you are feeling."),
-        # p("- Lower seizure threshold: This combination of medication may possibly increase risk of seizures for patients who already have a seizure disorder."),
-        # p("- QT prolongation: This can occur when the heart muscle takes a longer time to contract than to relax. Taking two or more medications with this risk can lead to an abnormal heart rhythm. Report any chest pain or palpitations to your physician immediately."),
-        # p("- Extrapyramidal symptoms: This combination of medications may lead to increased risk of developing a movement disorder. Report any abnormal or uncontrolled movements to your prescriber."),
-        # p("- Peripheral edema: Depakote and risperidone (a type of antipsychotic) together can lead to swelling of the limbs. Contact your prescriber if you notice any swelling in your limbs."),
         
         h3("Possible medication combinations include:"),
         tableOutput("table")
@@ -76,9 +66,8 @@ ui <- fluidPage(
 # Define server logic required to create table of combinations
 server <- function(input, output, session) {
 
-  # run each time a user changes text
-  output$table <- renderTable({
- 
+  observeEvent(input$medicationsPsychiatry, {
+    
     # source vector       
     meds <- as.character(input$medicationsPsychiatry)
     
@@ -87,39 +76,48 @@ server <- function(input, output, session) {
     
     # size of target vectors
     r <- input$n_combo
-
-    # string to filter medication names
-    filter_med1 <- stringr::str_to_lower(input$filter_med)
     
-    # create combinations of all possible entries
-    tibble_combo <- tibble::as_tibble(
-      gtools::combinations(n=n, r=r, v = meds, set = TRUE, repeats.allowed = FALSE)
-    )
-    names(tibble_combo) <- paste("Medication", 1:r)
+    if(n >= 2){
+      
+      # run each time a user changes text
+      output$table <- renderTable({
     
-    # if text is provided for filtering by medication...
-    if(!is.null(filter_med1)){
-      # ...then filter medicine by that medication
-      tibble_combo <- dplyr::filter(
-        .data = tibble_combo,
-        dplyr::if_any(.cols = dplyr::everything(), .fns = ~ grepl(filter_med1, .))
-      )
+        # string to filter medication names
+        filter_med1 <- str_to_lower(input$filter_med)
+        
+        # create combinations of all possible entries
+        tibble_combo <- as_tibble(
+          combinations(n=n, r=r, v = meds, set = TRUE, repeats.allowed = FALSE)
+        )
+        names(tibble_combo) <- paste("Medication", 1:r)
+        
+        # if text is provided for filtering by medication...
+        if(!is.null(filter_med1)){
+          # ...then filter medicine by that medication
+          tibble_combo <- filter(
+            .data = tibble_combo,
+            if_any(.cols = everything(), .fns = ~ grepl(filter_med1, .))
+          )
+        }
+        
+        # format table for document
+        tibble_out <- mutate(
+          # combine multiple medications into single column
+          unite(
+            data = tibble_combo, 
+            col = med_combo, sep = " + "
+          ), 
+          # capitalize first letter of each line
+          med_combo = gsub("^([a-z])", "\\U\\1", med_combo, perl=TRUE)
+        )
+        
+        # final output
+        names(tibble_out) <- paste("Combinations of", r, "Medications")
+        tibble_out
+        
+      })
+      
     }
-    
-    # format table for document
-    tibble_out <- dplyr::mutate(
-      # combine multiple medications into single column
-      tidyr::unite(
-        data = tibble_combo, 
-        col = med_combo, sep = " + "
-      ), 
-      # capitalize first letter of each line
-      med_combo = gsub("^([a-z])", "\\U\\1", med_combo, perl=TRUE)
-    )
-    
-    # final output
-    names(tibble_out) <- paste("Combinations of", r, "Medications")
-    tibble_out
     
   })
     
